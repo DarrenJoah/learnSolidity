21cd4f0e19a60e151ea75d182823b6f32a3bfe00
DarrenJoah <36428403+DarrenJoah@users.noreply.github.com>
2023-08-10 11:31:42 +0800

    Create StorageFactory.sol

---
+++ contracts/StorageFactory.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./SimpleStorage.sol";
+
+contract StorageFactory {
+    SimpleStorage[] public simpleStorageArray;
+
+    function createSimpleStorageContract() public {
+        //How does storage factory know whate simple storage looks like?
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+
+    }
+    //StorageFactory contract call the store function the SimpleStorage contarct
+    function sfStore(uint256 _simpleStoreIndex, uint256 _simpleStorageNumber) public {
+        //和其他合约进行交互需要Address,ABI(Application Binary Interface)
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStoreIndex];
+        //SimpleStorage simpleStorage = SimpleStorage(simpleStorageArray[_simpleStoreIndex]);
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256){
+        //SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+         //return simpleStorage.retrieve();
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
+       
+    }
+}
