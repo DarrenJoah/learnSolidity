e4bd54522d706517ea189954584c86009887a900
DarrenJoah <36428403+DarrenJoah@users.noreply.github.com>
2023-08-10 17:02:22 +0800

    Create FundMeUsingLibrary.sol

---
+++ contracts/FundMeUsingLibrary.sol
@@ -0,0 +1,29 @@
+//Get funds from users
+//Withdraw funds
+//Set a minmum funding value in USD
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.8;
+
+import "./PriceConverter.sol";
+//AggregatorV3Interface接口的github地址
+//https://github.com/smartcontractkit/chainlink/blob/develop/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol
+
+contract FundMeUsingLibrary {
+
+    using PriceConverter for uint256;
+    uint256 public minimumUsd = 50 * 1e18;
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
+    function fund() public payable {
+        //Want to be able to set a minimun fund amount in USD
+        //1. How do wo send ETH to this contract?
+        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+    //function withdraw(){}
+}
