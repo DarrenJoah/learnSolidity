b48b11a4d0b82bace932d08ac94c1373b6082c44
DarrenJoah <zhangchao_cun@163.com>
2023-08-15 19:20:07 +0800

    encrytedjson

--- ethers-simple-storage-fcc/deploy.js
+++ ethers-simple-storage-fcc/deploy.js
@@ -8,14 +8,23 @@ require("dotenv").config();
  * Description: 创建工厂合约后，使用工厂合约部署SimpleStorage合约到本地的Ganache工具的以太坊测试网络
  */
 
-const privateKey = process.env.PRIVATE_KEY;
+//const privateKey = process.env.PRIVATE_KEY;
 async function main() {
   //ganache运行的区块链的rpc server:http://127.0.0.1:7545
   //连接到本地区块链
   const provider = new ethers.providers.JsonRpcProvider(
     "http://127.0.0.1:7545"
   );
-  const wallet = new ethers.Wallet(privateKey, provider);
+  //const wallet = new ethers.Wallet(privateKey, provider);
+  const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
+  console.log(encryptedJson);
+  console.log(process.env.PRIVATE_KEY_PASSWORD);
+  let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+    encryptedJson,
+    process.env.PRIVATE_KEY_PASSWORD
+  );
+  wallet = await wallet.connect(provider);
+
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
   const binary = fs.readFileSync(
     "./SimpleStorage_sol_SimpleStorage.bin",
