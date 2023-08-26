a427ce01c75eeb47d02b0c5bf55c9f28ce7b415c
DarrenJoah <zhangchao_cun@163.com>
2023-08-26 11:46:21 +0800

    verify contract by code

--- hardhat-simple-storage-fcc/scripts/deploy.js
+++ hardhat-simple-storage-fcc/scripts/deploy.js
@@ -1,4 +1,4 @@
-const { ethers } = require("hardhat");
+const { ethers, run, network } = require("hardhat");
 
 
 async function main() {
@@ -6,7 +6,34 @@ async function main() {
   console.log("Deploying contract...")
   const simpleStorage = await SimpleStorageFactory.deploy()
   await simpleStorage.deployed()
-  console.log(`Deploy contract to: ${simpleStorage.address} `)
+  console.log(`Deploy contract to: ${simpleStorage.address}`)
+  //console.log(network.config)
+  //4==4 true
+  //4 == "4" true
+  //4 === "4" false ,===不会进行类型转换
+  if(network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+    await simpleStorage.deployTransaction.wait(6)
+    await verify(simpleStorage.address, [])
+  }
+}
+
+//验证合约的函数
+async function verify(contractAddress, args) {
+  console.log("Verifying contract...")
+  try{
+    await run("verify:verify",{
+      address: contractAddress,
+      constructorArguments: args,
+    })
+  }
+  catch(e){
+    if(e.message.toLowerCase().includes("already verified")){
+      console.log("Already Verified!")
+    }
+    else{
+      console.log(e)
+    }
+  }
 }
 
 main()
