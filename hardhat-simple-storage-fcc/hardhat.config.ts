a59241b6370d4d4c7046aaeae444b0e4f53c8110
DarrenJoah <zhangchao_cun@163.com>
2023-08-30 23:51:17 +0800

    ts version

--- hardhat-simple-storage-fcc/hardhat.config.ts
+++ hardhat-simple-storage-fcc/hardhat.config.ts
@@ -1,19 +1,15 @@
-require("@nomiclabs/hardhat-waffle");
-require("dotenv").config();
-require("@nomicfoundation/hardhat-verify");
+import "@nomiclabs/hardhat-waffle";
+import "dotenv/config";
+import "@nomicfoundation/hardhat-verify";
+import "./tasks/block-number";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-ethers";
+import { task, HardhatUserConfig } from "hardhat/config";
 
-require("./tasks/block-number");
-require("hardhat-gas-reporter");
-require("solidity-coverage");
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
