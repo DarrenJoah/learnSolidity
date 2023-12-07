0e569e5fdef62b2a2054fcef541fa4fa2d6ee3df
DarrenJoah <36428403+DarrenJoah@users.noreply.github.com>
2023-12-07 10:08:51 +0800

    Create 函数定义顺序.sol

---
+++ yufa/函数定义顺序.sol
@@ -0,0 +1,24 @@
+    contract A {
+        function A() public {//构造函数
+            ...
+        }
+
+        function() public {//转账默认执行函数
+            ...
+        }
+
+        // External functions //外部函数
+        // ...
+
+        // External functions that are constant //外部constant函数
+        // ...
+
+        // Public functions //公共函数
+        // ...
+
+        // Internal functions //内部函数
+        // ...
+        // Private functions //私有函数
+
+        // ...
+    }
